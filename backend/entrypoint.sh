808d0a26406a8599f297b9fa9b5488f8026eed88
Кирилл <158262874+Kirill374mansurov@users.noreply.github.com>
2026-03-29 01:13:01 +0300

    Update entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,9 @@
-python manage.py makemigrations --no-input
-python manage.py migrate --no-input
+#!/bin/sh
+set -e
+
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 python manage.py run_import
-python manage.py collectstatic --no-input --clear
+python manage.py collectstatic --noinput
 cp -r /app/collected_static/. /backend_static/static/
-gunicorn --bind 0:8000 backend.wsgi
+gunicorn --bind 0.0.0.0:8000 backend.wsgigi
